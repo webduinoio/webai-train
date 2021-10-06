c01203d61adfdf061fe17a6d6f46513fdd6f8fa3
zz358840 <kevin@kingkit101.com>
2021-10-06 12:31:07 +0800

    feat: update save path

--- compilerDetectorModel.sh
+++ compilerDetectorModel.sh
@@ -1,8 +1,8 @@
 cd maix_train
 rm -rf out
 cp /content/drive/MyDrive/$1 ../kmodels
-mkdir -p /content/drive/MyDrive/webai-train
-cp /content/drive/MyDrive/$1 /content/drive/MyDrive/webai-train/$1
+mkdir -p /content/drive/MyDrive/webai-train/detector/$1
+cp /content/drive/MyDrive/$1 /content/drive/MyDrive/webai-train/detector/$1/$1
 python3 train.py init
 python3 train.py -t detector -z ../kmodels/$1 train
 python3 ../getKmodel.py $1 detector
