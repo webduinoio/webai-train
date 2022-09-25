739c8172b43f833c5c32e1b8a1f2770cfd80725f
marty <marty.shu@gmail.com>
2022-09-26 07:53:18 +0800

    Unzip the specified file, if the file does not exist, it will not be processed

--- cvtTMFile.py
+++ cvtTMFile.py
@@ -1,32 +1,34 @@
-# 解壓縮指定檔案
+# 解壓縮指定檔案，檔案如果不存在就不處理
 
 import argparse, os, sys, shutil
 import zipfile 
 import sys
 
 path = sys.argv[1]
-print('processing...',path)
-files = zipfile.ZipFile(path)
 
-try:	 
-	os.mkdir(path)
-except:
-	pass
+if os.path.isfile(path):
+	print('processing...',path)
+	files = zipfile.ZipFile(path)
 
-for i in files.namelist():
-	dir =''
-	filename = i
-	try:
-		idx = filename.index('-')
-	except:
-		continue
-	try:	
-		dir = filename[:idx]
-		os.mkdir(path+'.datasets/'+dir)
+	try:	 
+		os.mkdir(path)
 	except:
 		pass
-	try:
-		ext = filename.index('.jpg')
-		files.extract(filename, path+'.datasets/'+dir)
-	except:
-		pass
+
+	for i in files.namelist():
+		dir =''
+		filename = i
+		try:
+			idx = filename.index('-')
+		except:
+			continue
+		try:	
+			dir = filename[:idx]
+			os.mkdir(path+'.datasets/'+dir)
+		except:
+			pass
+		try:
+			ext = filename.index('.jpg')
+			files.extract(filename, path+'.datasets/'+dir)
+		except:
+			pass
