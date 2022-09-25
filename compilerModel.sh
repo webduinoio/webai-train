df0beca49686a617833f864b8144d55008875b1a
marty <marty.shu@gmail.com>
2022-09-26 07:50:59 +0800

    create kmodel zip file

--- compilerModel.sh
+++ compilerModel.sh
@@ -3,3 +3,6 @@ rm -rf out
 python3 ../cvtTMFile.py ../kmodels/$1
 python3 train.py -t classifier -d ../kmodels/$1.datasets train
 python3 ../getKmodel.py ../kmodels/$1
+tensorflowjs_converter --input_format=keras ./out/m.tflite.h5 ../kmodels/
+zip ../kmodels/model.kfpkg ../flash-list.json ../kmodels/project.tm.kmodel
+echo '100%'
